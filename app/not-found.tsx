6dea26b72d1e51508ebae43661d25197ef7996c7
Noa.O <noa.obringer@gmail.com>
2025-03-03 12:25:33 +0100

    fix: 404 height

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,6 +15,11 @@ const NotFound = () => {
       <div style={{ position: 'absolute', bottom: 200, left: 0, width: '100%', height: '100%', zIndex: 0 }}>
         <Scene404 />
       </div>
+      <div className='not-found-container'>
+        <div className='not-found-text'>
+            <p><Link className='a' href="/"> HomePage</Link>!</p>
+        </div>  
+      </div>
       <Footer />
     </main>
     </>
